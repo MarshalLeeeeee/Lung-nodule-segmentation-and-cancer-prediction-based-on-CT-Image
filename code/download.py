b7771b0b83e3693bd95743f33103752366632a9a
marshallee <marshallee413lmc@sina.com>
2018-06-16 11:16:08 +0800

    upload download script

---
+++ code/download.py
@@ -0,0 +1,26 @@
+import sys
+import os 
+from datetime import datetime
+
+file_id = [
+'https://drive.google.com/open?id=0Bz-jINrxV740dVhQRHl6OWNRanc', 
+'https://drive.google.com/open?id=0Bz-jINrxV740QTZNenZadmw4NEE',
+'https://drive.google.com/open?id=0Bz-jINrxV740OFRGZ0lvZ1pZU28',
+'https://drive.google.com/open?id=0Bz-jINrxV740Z0NoSW9JRE9RRVk',
+'https://drive.google.com/open?id=0Bz-jINrxV740SDB3R0hYalJGa2M',
+'https://drive.google.com/open?id=0Bz-jINrxV740Nm1fQWhTZUVia2s',
+'https://drive.google.com/open?id=0Bz-jINrxV740YXBpYV9MQkVxSk0',
+'https://drive.google.com/open?id=0Bz-jINrxV740YTM0X2dQcGZ1eFU',
+'https://drive.google.com/open?id=0Bz-jINrxV740WEpXakYyRWxaSm8',
+'https://drive.google.com/open?id=0Bz-jINrxV740YVZFMEdpQmZrOTQ'
+]
+
+file_id = [fid[fid.find('=')+1:] for fid in file_id]
+
+file_name = ['subset'+str(i)+'.zip' for i in range(1, 10)]
+
+for fid, fname in zip(file_id, file_name):
+	print fname 
+	print datetime.now()
+	command = 'python google_drive.py '+fid+ ' '+fname 
+	os.sys(command)
